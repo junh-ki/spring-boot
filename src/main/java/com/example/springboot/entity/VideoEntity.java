50adb13af8063bd5f92901633227540c1d9871a6
Junhyung Ki <kijoonh91@gmail.com>
2023-02-12 11:21:25 +0100

    Set up H2 in-memory db (WIP)

--- src/main/java/com/example/springboot/entity/VideoEntity.java
+++ src/main/java/com/example/springboot/entity/VideoEntity.java
@@ -3,11 +3,15 @@ package com.example.springboot.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name = "video")
+@AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class VideoEntity {
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO video (id, name, description)
+VALUES (1, 'Interstellar', 'Very good movie'), (2, 'Jane Eyre', 'Have not seen it yet'),
+       (3, 'Super Bad', 'Very funny movie'), (4, 'The Amazing Spider-man', 'Amazing movie');
+-- CREATE TABLE video (id big_int primary key auto_increment, name varchar(255), description text);
